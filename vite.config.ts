0a33023927e2e7d1040472b53503840707d3a934
patriciodunstan <patricio.dunstan@gmail.com>
2026-01-20 01:19:53 -0300

    feat: Configure Vite to load and expose environment variables via `process.env`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-    css: true,
-  },
+    define: {
+      "process.env": env,
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+      css: true,
+    },
+  };
 });
